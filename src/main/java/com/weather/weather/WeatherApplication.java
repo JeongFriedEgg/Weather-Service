81667134640c51d17c970f211195a630c65a0c57
jeongbanseog <wibalislfilask@gmail.com>
2025-04-17 08:37:06 +0900

    feat : 스케쥴링 기능 구현

--- src/main/java/com/weather/weather/WeatherApplication.java
+++ src/main/java/com/weather/weather/WeatherApplication.java
@@ -2,8 +2,12 @@ package com.weather.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableTransactionManagement
 public class WeatherApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/weather/weather/service/WeatherService.java
+++ src/main/java/com/weather/weather/service/WeatherService.java
@@ -4,11 +4,12 @@ import com.weather.weather.domain.DateWeather;
 import com.weather.weather.infrastructure.client.OpenWeatherMapClient;
 import com.weather.weather.repository.DateWeatherRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +17,12 @@ public class WeatherService {
 
     private final OpenWeatherMapClient openWeatherMapClient;
     private final DateWeatherRepository dateWeatherRepository;
+
+    @Scheduled(cron = "0 0 1 * * *")
+    @Transactional
+    public void saveWeatherDate() {
+        dateWeatherRepository.save(openWeatherMapClient.getWeatherFromApi());
+    }
 
     public DateWeather getDateWeather(LocalDate date) {
         List<DateWeather> dateWeatherListFromDB = dateWeatherRepository.findAllByDate(date);
